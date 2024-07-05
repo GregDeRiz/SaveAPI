b568b5ea06a1df458301f614e000ef39ab545021
BuildTools <unconfigured@null.spigotmc.org>
2024-07-05 02:14:49 +0200

    Added player stat saving and restoring

--- src/main/java/fr/gregderiz/saveapi/PlayersManager.java
+++ src/main/java/fr/gregderiz/saveapi/PlayersManager.java
@@ -1,4 +1,31 @@
 package fr.gregderiz.saveapi;
 
-public class PlayerManager {
+import com.google.common.collect.Maps;
+import org.bukkit.entity.Player;
+
+import java.util.Map;
+import java.util.UUID;
+
+public final class PlayersManager {
+    private final Map<UUID, Stat> playerStats = Maps.newHashMap();
+
+    public boolean isPlayerSaved(Player player) {
+        return this.playerStats.containsKey(player.getUniqueId());
+    }
+
+    public void savePlayerStat(Player player, Stat stat) {
+        if (isPlayerSaved(player)) return;
+
+        this.playerStats.put(player.getUniqueId(), stat);
+    }
+
+    public void restorePlayerStat(Player player) {
+        if (!isPlayerSaved(player)) return;
+
+        this.playerStats.remove(player.getUniqueId());
+    }
+
+    public void restorePlayerStats() {
+        playerStats.forEach((uuid, stat) -> stat.restore(null));
+    }
 }
--- src/main/java/fr/gregderiz/saveapi/Stat.java
+++ src/main/java/fr/gregderiz/saveapi/Stat.java
@@ -1,2 +1,47 @@
-package fr.gregderiz.saveapi;public class Stat {
+package fr.gregderiz.saveapi;
+
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Stat {
+    private Location previousLocation;
+    private GameMode previousGameMode;
+    private ItemStack[] previousInventory;
+    private ItemStack[] previousArmor;
+
+    private final Player player;
+    private final PlayersManager playersManager;
+
+    public Stat(Player player, PlayersManager playersManager) {
+        this.player = player;
+        this.playersManager = playersManager;
+    }
+
+    public void save() {
+        this.previousLocation = this.player.getLocation();
+        this.previousGameMode = this.player.getGameMode();
+        this.previousInventory = this.player.getInventory().getContents();
+        this.previousArmor = this.player.getInventory().getArmorContents();
+
+        this.player.getInventory().clear();
+        this.playersManager.savePlayerStat(this.player, this);
+    }
+
+    public void restore(JavaPlugin plugin) {
+        this.player.getInventory().clear();
+
+        if (this.previousLocation != null) this.player.teleport(this.previousLocation);
+        if (this.previousGameMode != null) this.player.setGameMode(this.previousGameMode);
+        if (this.previousInventory != null) this.player.getInventory().setContents(this.previousInventory);
+        if (this.previousArmor != null) this.player.getInventory().setArmorContents(this.previousArmor);
+
+        this.playersManager.restorePlayerStat(this.player);
+        if (plugin == null) return;
+
+        Bukkit.getServer().getScheduler().runTaskLater(plugin, () -> this.player.setFireTicks(0), 2);
+    }
 }
